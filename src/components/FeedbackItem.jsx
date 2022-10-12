bee42fb2c0b47c161081471eed9fb988d69bbd18
Shawn Witemyre <switemyre@comcast.net>
2022-10-11 21:58:18 -0400

    Added context and edit feedback functionality

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-export default function FeedbackItem({ item, reverse, handleDelete }) {
+export default function FeedbackItem({ item, reverse }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
   return (
     <Card reverse={reverse}>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
+      <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button onClick={() => editFeedback(item)} className="edit">
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
